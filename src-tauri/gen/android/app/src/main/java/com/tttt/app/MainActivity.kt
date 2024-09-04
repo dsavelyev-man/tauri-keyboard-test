31675297d8d31d6867246dbb1cdb82cd8d9e70eb
dsavelyev <kriper0nind@gmail.com>
2024-09-04 09:26:46 +0300

    check keyboard

--- src-tauri/gen/android/app/src/main/java/com/tttt/app/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/tttt/app/MainActivity.kt
@@ -1,3 +1,24 @@
 package com.tttt.app
 
-class MainActivity : TauriActivity()
+import android.webkit.WebView
+import androidx.lifecycle.Observer
+
+class MainActivity : TauriActivity() {
+  private var keyboardTriggerBehavior: KeyboardTriggerBehavior? = null
+
+  override fun onWebViewCreate(webView: WebView) {
+    super.onWebViewCreate(webView)
+    //          webView.loadUrl("javascript:window.keyboardStatusChanged(true)")
+
+    val activity = this ?: return
+    keyboardTriggerBehavior = KeyboardTriggerBehavior(activity, 120).apply {
+      observe(activity, Observer {
+        when (it) {
+          KeyboardTriggerBehavior.Status.OPEN -> webView.loadUrl("javascript:keyboardStatusChanged(true)")
+          KeyboardTriggerBehavior.Status.CLOSED ->  webView.loadUrl("javascript:keyboardStatusChanged(false)")
+        }
+      })
+    }
+//    webView.loadUrl("javascript:alert('test')")
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,14 @@
-import { useEffect } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { listen, TauriEvent } from "@tauri-apps/api/event";
-import { invoke } from "@tauri-apps/api/core";
-import pino from "pino";
-import { createPortal } from "react-dom/client";
 
-const logger = pino({
-  browser: {
-    serialize: true,
-    asObject: true,
-    write: {
-      info: (o) => {
-        const div = document.createElement("div");
-
-        //@ts-ignore
-        div.innerText = JSON.stringify(o, null, 2);
-
-        document.body.appendChild(div);
-      },
-    },
-  },
-});
+//@ts-ignore
+window.keyboardStatusChanged = (value: boolean) => {
+  alert(value);
+};
 
 function App() {
-  useEffect(() => {
-    setInterval(() => {
-      logger.info(window.visualViewport.height);
-    }, 5000);
-  }, []);
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    invoke("greet", {
-      name: "tres",
-    });
-    invoke("download", {
-      url: "tres",
-    });
-  }
-
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input id="greet-input" placeholder="Enter a name..." />
-        <button type="submit">Greet</button>
-      </form>
+      <input placeholder="input" />
     </div>
   );
 }
